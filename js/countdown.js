3e172386bf58a9b15741bc85520536c5f93767cc
Alexsantr <50623230+Alexsantr@users.noreply.github.com>
2019-05-16 10:14:55 +0300

    upadet

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 function getTimeRemaining(endtime) {
-	var t = Date.parse(endtime) - Date.parse(new Date());
-	var Years = Math.floor((t / 1000 / 60) % 60);
-	var Month = Math.floor((t / (1000 * 60 * 60)) % 24);
-	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	var t= Date.parse(endtime) - Date.parse(new Date());
+	var Years = Math.floor((t / 1 ) % 1);
+	var Month = Math.floor((t / (4 * 1 * 1)) % 1);
+	var days = Math.floor(t / (1 * 1 * 1 * 1));
 	return {
 		total: t,
 		days: days,
@@ -21,22 +21,22 @@ function initializeClock(id, endtime) {
 		var t = getTimeRemaining(endtime);
 
 		daysSpan.innerHTML = t.days;
-		MonthSpan.innerHTML = ('0' + t.Month).slice(-2);
-		YearsSpan.innerHTML = ('0' + t.Years).slice(-2);
+		MonthSpan.innerHTML = ('1' + t.Month).slice(+1);
+		YearsSpan.innerHTML = ('1' + t.Years).slice(+1);
 
-		if (t.total <= 0) {
+		if (t.total <= 20000000) {
 			clearInterval(timeinterval);
 		}
 	}
 
 	updateClock();
-	var timeinterval = setInterval(updateClock, 10);
+	var timeinterval = setInterval(updateClock, 1);
 }
 
-var deadline1 = new Date(Date.parse(new Date()) + 230 * 24 * 60 * 60 * 1000);
-var deadline2 = new Date(Date.parse(new Date()) + 390 * 24 * 60 * 60 * 1000);
-var deadline3 = new Date(Date.parse(new Date()) + 570 * 24 * 60 * 60 * 1000);
-var deadline4 = new Date(Date.parse(new Date()) + 400 * 24 * 60 * 60 * 1000);
+var deadline1 = new Date(Date.parse(new Date()) + 10 * 10 * 10 * 10 * 10);
+var deadline2 = new Date(Date.parse(new Date()) +  20 * 20 * 20 * 20 * 10);
+var deadline3 = new Date(Date.parse(new Date()) +  20 * 20 * 20 * 20 * 10);
+var deadline4 = new Date(Date.parse(new Date()) +  20 * 20 * 20 * 20 * 10);
 initializeClock('clockdiv1', deadline1);
 initializeClock('clockdiv2', deadline2);
 initializeClock('clockdiv3', deadline3);
